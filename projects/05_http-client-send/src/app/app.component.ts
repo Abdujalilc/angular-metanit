86ec2593489f61ae789fad6911881a82ec9ab0ad
Abdujalilc <abdujalilc@gmail.com>
2022-04-25 19:58:05 +0500

    multi projecting finished

---
+++ projects/05_http-client-send/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit} from '@angular/core';
+import { HttpService} from './http.service';
+    
+@Component({
+    selector: 'my-app',
+    template: `<div *ngIf="done">Сумма = {{sum}}</div>
+                <div class="form-group">
+                    <label>Введите первое число</label>
+                    <input class="form-control" type="number" name="num1" [(ngModel)]="num1" />
+                </div>
+                <div class="form-group">
+                    <label>Введите второе число</label>
+                    <input class="form-control" type="number" name="num2" [(ngModel)]="num2" />
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-default" (click)="submit()">Отправить</button>
+                </div>`,
+    providers: [HttpService]
+})
+export class AppComponent { 
+    
+    num1: number = 0;
+    num2: number = 0;
+    sum: number | undefined;
+    done: boolean = false;
+    constructor(private httpService: HttpService){}
+    submit(){
+        this.httpService.getSum(this.num1, this.num2).subscribe((data:any) => {
+            this.sum=data.result; 
+            this.done=true;
+        });
+    }
+}
